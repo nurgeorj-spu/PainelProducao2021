9514ba7c649d6766a2cf596fd7c0eca179c8231e
Raphael Coelho <raphaelcoelhorj@hotmail.com>
2021-04-12 20:13:14 -0300

    Atualização dos tipos de campos

--- omtg-ddl-structure.sql
+++ omtg-ddl-structure.sql
@@ -1,4 +1,4 @@
---------------------- 11/02/2021 -------------------
+--------------------- 12/04/2021 -------------------
 
 -- Create table tipoAcao
 CREATE TABLE tipoAcao (
@@ -17,18 +17,23 @@ CREATE TABLE profissional (
 
 -- Create table CDG
 CREATE TABLE CDG (
-  idNacional INTEGER NOT NULL,
-  nomeProduto VARCHAR(250) NOT NULL,
+  idNacional VARCHAR(7) NOT NULL,
   uf VARCHAR(2) NOT NULL,
+  cadastrador VARCHAR(250) NOT NULL,
+  prodLocali VARCHAR(3) NOT NULL,
+  nomeProduto VARCHAR(250),  
   producao VARCHAR(100),
   PatriArmazen VARCHAR(100),
-  Nprocesso VARCHAR(100),
-  tipoCDG VARCHAR(50) NOT NULL,
-  NomeColecao VARCHAR(250),
-  conceituacao VARCHAR(100),
-  Ano INTEGER,
-  EstruturaOrig VARCHAR(50),
+  nProcesso VARCHAR(100),
+  formatoCDG VARCHAR(50),
+  produtCDG VARCHAR(50), 
+  nomeColecao VARCHAR(250),
+  tipoBemUni VARCHAR(100),
+  Ano DATE,
   codMunicipio INTEGER,
+  coordPresen BOOLEAN,
+  EPSGpres VARCHAR(30),
+  EPSG INTEGER,
   escala INTEGER,
   CONSTRAINT pk_CDG PRIMARY KEY (idNacional)
 );
@@ -36,7 +41,8 @@ CREATE TABLE CDG (
 -- Create table procedmetodolog
 CREATE TABLE procedmetodolog (
   codProced INTEGER NOT NULL,
-  fk_idNacional INTEGER NOT NULL,
+  dataP DATE,
+  fk_idNacional VARCHAR(7) NOT NULL,
   fk_codProf INTEGER NOT NULL,
   fk_codAcao INTEGER NOT NULL,
   CONSTRAINT pk_procedmetodolog PRIMARY KEY (codProced),
@@ -48,9 +54,9 @@ CREATE TABLE procedmetodolog (
 -- Create table georreferenciamento
 CREATE TABLE georreferenciamento (
   codGeorr INTEGER NOT NULL,
-  parecerGeorref VARCHAR(3),
-  parecerVerificacao VARCHAR(3),
-  fk_idNacional_geor INTEGER NOT NULL,
+  parecer VARCHAR(25),
+  verificacao VARCHAR(25),
+  fk_idNacional_geor VARCHAR(7) NOT NULL,
   CONSTRAINT pk_georreferenciamento PRIMARY KEY (codGeorr),
   CONSTRAINT fk_georreferenciamento_ref_CDG FOREIGN KEY (fk_idNacional_geor) REFERENCES CDG(idNacional) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -58,10 +64,19 @@ CREATE TABLE georreferenciamento (
 -- Create table geolocalizacao
 CREATE TABLE geolocalizacao (
   codGeoloc INTEGER NOT NULL,
-  parecerGeoloc VARCHAR(3),
-  fk_idNacional_geol INTEGER,
+  parecer VARCHAR(25),
+  fk_idNacional_geol VARCHAR(7) NOT NULL,
   CONSTRAINT pk_geolocalizacao PRIMARY KEY (codGeoloc),
   CONSTRAINT fk_geolocalizacao_ref_CDG FOREIGN KEY (fk_idNacional_geol) REFERENCES CDG(idNacional) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Create table geoaproximacao
+CREATE TABLE geoaproximacao (
+  codapro INTEGER NOT NULL,
+  parecer VARCHAR(25),
+  fk_idNacional_apro VARCHAR(7) NOT NULL, 
+  CONSTRAINT pk_geoaproximacao PRIMARY KEY (codapro),
+  CONSTRAINT fk_geoaproximacao_ref_CDG FOREIGN KEY (fk_idNacional_apro) REFERENCES CDG(idNacional) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table LogBD
@@ -70,7 +85,7 @@ CREATE TABLE LogBD (
   dataAlteracao DATE NOT NULL,
   Usuario VARCHAR(50),
   Operacao VARCHAR(10),
-  fk_idNacional_log INTEGER NOT NULL,
+  fk_idNacional_log VARCHAR(7) NOT NULL,
   CONSTRAINT pk_LogBD PRIMARY KEY (codLog),
   CONSTRAINT fk_LogBD_ref_CDG FOREIGN KEY (fk_idNacional_log) REFERENCES CDG(idNacional)
 );
@@ -82,7 +97,7 @@ CREATE OR REPLACE FUNCTION "Producao".f_trigger_ins_geolocalizacao()
  LANGUAGE plpgsql
 AS $function$
 
--- Propósito: verifica se o valor informado para o campo fk_idNacional já existe na tabela georreferenciamento
+-- Proposito: verifica se o valor informado para o campo fk_idNacional ja existe na tabela georreferenciamento
 
 BEGIN
  -- regra 
@@ -93,7 +108,7 @@ BEGIN
    -- do contrario ...
    ELSE
    -- rejeita!
-   RAISE EXCEPTION 'O Valor %',  NEW.fk_idNacional  || ', fornecido para o campo idNacional, já está em uso na tabela georreferenciamento.' USING HINT = 'Corrija e tente novamente';
+   RAISE EXCEPTION 'O Valor %',  NEW.fk_idNacional  || ', fornecido para o campo idNacional, jÃ¡ estÃ¡ em uso na tabela georreferenciamento.' USING HINT = 'Corrija e tente novamente';
  END IF;
 END $function$
 ;
@@ -115,7 +130,7 @@ CREATE OR REPLACE FUNCTION "Producao".f_trigger_ins_georreferenciamento()
  LANGUAGE plpgsql
 AS $function$
 
--- Propósito: verifica se o valor informado para o campo fk_idNacional já existe na tabela geolocalizacao
+-- Propï¿½sito: verifica se o valor informado para o campo fk_idNacional jï¿½ existe na tabela geolocalizacao
 
 BEGIN
  -- regra 
@@ -126,7 +141,7 @@ BEGIN
    -- do contrario ...
    ELSE
    -- rejeita!
-   RAISE EXCEPTION 'O Valor %',  NEW.fk_idNacional  || ', fornecido para o campo idNacional, já está em uso na tabela geolocalização.' USING HINT = 'Corrija e tente novamente';
+   RAISE EXCEPTION 'O Valor %',  NEW.fk_idNacional  || ', fornecido para o campo idNacional, jï¿½ estï¿½ em uso na tabela geolocalizaï¿½ï¿½o.' USING HINT = 'Corrija e tente novamente';
  END IF;
 END $function$
 ;
@@ -159,29 +174,29 @@ CREATE or REPLACE FUNCTION "Producao".f_trigger_logbd() RETURNS trigger
     AS $$
 
 
-/*-- Propósito: Insere na tabela logBD todas as operções realizadas na entidade CDG.
-A função verifica uma cadeia de caracteres contendo INSERT, UPDATE, ou DELETE, e informa para qual operação o gatilho foi disparado (Tipo de dado text).*/
+/*-- PropÃ³sito: Insere na tabela logBD todas as operaÃ§Ãµes realizadas na entidade CDG.
+A funÃµes verifica uma cadeia de caracteres contendo INSERT, UPDATE, ou DELETE, e informa para qual operaÃ§Ãµes o gatilho foi disparado (Tipo de dado text).*/
 	
 BEGIN
 	IF (TG_OP = 'INSERT') THEN
 		INSERT INTO "Producao".logbd
 		(CodLog, fk_idnacional_log, DataAlteracao, Usuario, Operacao) 
 		VALUES
-		(NEXTVAL('"Producao".logbd_seq'), new.fk_idnacional_log, current_timestamp, current_user, 'INSERT');
+		(NEXTVAL('"Producao".logbd_seq'), new.idnacional, current_timestamp, current_user, 'INSERT');
 		RETURN NEW;
 	
 	ELSIF (TG_OP = 'DELETE') THEN
 		INSERT INTO "Producao".logbd
 		(CodLog, fk_idnacional_log, DataAlteracao, Usuario, Operacao) 
 		VALUES
-		(NEXTVAL('"Producao".logbd_seq'), OLD.fk_idnacional_log, current_timestamp, current_user, 'DELETE');
+		(NEXTVAL('"Producao".logbd_seq'), OLD.idnacional, current_timestamp, current_user, 'DELETE');
 		RETURN OLD;
 		
 	ELSIF (TG_OP = 'UPDATE') THEN
 		INSERT INTO "Producao".logbd
 		(CodLog, fk_idnacional_log, DataAlteracao, Usuario, Operacao)
 		VALUES
-		(NEXTVAL('"Producao".logbd_seq'), NEW.fk_idnacional_log, current_timestamp, current_user, 'UPDATE');
+		(NEXTVAL('"Producao".logbd_seq'), NEW.idnacional, current_timestamp, current_user, 'UPDATE');
 		RETURN NEW;		
 				
 	END IF;
@@ -189,7 +204,7 @@ BEGIN
 END;
 $$;
 
-CREATE TRIGGER t_ins_logbd BEFORE INSERT OR DELETE OR UPDATE ON "Producao".CDG FOR EACH ROW EXECUTE PROCEDURE "Producao".f_trigger_logbd();
+CREATE TRIGGER t_ins_logbd AFTER INSERT OR DELETE OR UPDATE ON "Producao".CDG FOR EACH ROW EXECUTE PROCEDURE "Producao".f_trigger_logbd();
 
 
 ALTER FUNCTION producao_cartografica_spu.f_trigger_log_completo_folha() OWNER TO postgres;
